45c42f1385a35edf522f2c4e1372431506b5c356
immonroe <immanuelmonroe@gmail.com>
2025-03-21 15:30:39 -0400

    Added event listeners for each li

--- js/main.js
+++ js/main.js
@@ -14,13 +14,59 @@
 
 // There will be a start button to reset the board and start game from the beginning
 
+const listOne = document.querySelector('.one')
+listOne.addEventListener('click', () => {
+    console.log('You clicked on one!')
+})
+
+const listTwo = document.querySelector('.two')
+listTwo.addEventListener('click', () => {
+    console.log('You clicked on two!')
+})
+
+const listThree = document.querySelector('.three')
+listThree.addEventListener('click', () => {
+    console.log('You clicked on three!')
+})
+
+const listFour = document.querySelector('.four')
+listFour.addEventListener('click', () => {
+    console.log('You clicked on four!')
+})
+
+const listFive = document.querySelector('.five')
+listFive.addEventListener('click', () => {
+    console.log('You clicked on five!')
+})
+
+const listSix = document.querySelector('.six')
+listSix.addEventListener('click', () => {
+    console.log('You clicked on six!')
+})
+
+const listSeven = document.querySelector('.seven')
+listSeven.addEventListener('click', () => {
+    console.log('You clicked on seven!')
+})
+
+const listEight = document.querySelector('.eight')
+listEight.addEventListener('click', () => {
+    console.log('You clicked on eight!')
+})
+
+const listNine = document.querySelector('.nine')
+listNine.addEventListener('click', () => {
+    console.log('You clicked on nine!')
+})
+
 class Player {
     constructor(name, symbol) {
         this.name = name
         this.symbol = symbol
     }
 }
-const playerOne = prompt('Enter your name')
+
+const playerOne = 'Manny' // prompt('Enter your name') 
 
 let one = new Player(playerOne, 'X')
-let two = new Player(playerOne, 'O')
+let two = new Player('cpu', 'O')
